32fa7d0b9ed056700a902b781774cf96e92c94cd
asywalulfikri <asywalulfikri@gmail.com>
2024-09-21 23:05:28 +0700

    update aja

--- soundrecorder/src/main/java/sound/recorder/widget/notification/FirebaseMessageReceiver.kt
+++ soundrecorder/src/main/java/sound/recorder/widget/notification/FirebaseMessageReceiver.kt
@@ -11,6 +11,9 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import sound.recorder.widget.R
 
 class FirebaseMessageReceiver : FirebaseMessagingService() {
@@ -22,56 +25,52 @@ class FirebaseMessageReceiver : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d("ResponseNotification", remoteMessage.toString())
-        if (remoteMessage.data.isNotEmpty() || remoteMessage.notification != null) {
-            showNotification(
-                remoteMessage.notification?.title.toString(),
-                remoteMessage.notification?.body.toString()
-            )
+        // Gunakan Coroutine untuk memproses pesan di background
+        CoroutineScope(Dispatchers.IO).launch {
+            if (remoteMessage.data.isNotEmpty() || remoteMessage.notification != null) {
+                showNotification(
+                    remoteMessage.notification?.title.toString(),
+                    remoteMessage.notification?.body.toString()
+                )
+            }
         }
     }
 
     @SuppressLint("UnspecifiedImmutableFlag")
     fun showNotification(title: String?, message: String?) {
-        val intent = Intent()
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        val pendingIntent: PendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-        } else {
-            PendingIntent.getActivity(
-                this,
-                0, intent, PendingIntent.FLAG_UPDATE_CURRENT
-            )
-        }
-        val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val builder = NotificationCompat.Builder(applicationContext, channelId)
-            .setAutoCancel(true)
-            .setContentText(message)
-            .setSound(soundUri)
-            .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
-            .setContentTitle(title)
-            .setVibrate(
-                longArrayOf(
-                    1000, 1000, 1000,
-                    1000, 1000
+        try {
+            val intent = Intent()
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            val pendingIntent: PendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+            } else {
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            }
+
+            val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            val builder = NotificationCompat.Builder(applicationContext, channelId)
+                .setAutoCancel(true)
+                .setContentText(message)
+                .setSound(soundUri)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                .setContentTitle(title)
+                .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent)
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val notificationChannel = NotificationChannel(
+                    channelId, "web_app",
+                    NotificationManager.IMPORTANCE_HIGH
                 )
-            )
-            .setOnlyAlertOnce(true)
-            .setContentIntent(pendingIntent)
-
-        val notificationManager = getSystemService(
-            NOTIFICATION_SERVICE
-        ) as NotificationManager
-
-        // Check if the Android Version is greater than Oreo
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                channelId, "web_app",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationManager.createNotificationChannel(
-                notificationChannel
-            )
+                notificationManager.createNotificationChannel(notificationChannel)
+            }
+            notificationManager.notify(0, builder.build())
+        }catch (e : Exception){
+            Log.d("noo","noo")
         }
-        notificationManager.notify(0, builder.build())
+
     }
 }
