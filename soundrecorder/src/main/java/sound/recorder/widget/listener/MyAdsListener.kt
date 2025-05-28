087893fbbb71bd5a5c3f3b9ede1487a875d5cf89
asywalulfikri <asywalulfikri@gmail.com>
2025-05-28 12:14:10 +0700

    fixs lagi sueee

--- soundrecorder/src/main/java/sound/recorder/widget/listener/MyAdsListener.kt
+++ soundrecorder/src/main/java/sound/recorder/widget/listener/MyAdsListener.kt
@@ -16,5 +16,8 @@ object MyAdsListener {
     fun setShowButtonHome(show : Boolean) {
         myListener?.onViewButtonHome(show)
     }
+    fun setOnShowInterstitial() {
+        myListener?.onShowInterstitial()
+    }
 
 }
